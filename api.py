7bde3fa006b29eb4d6cbad130e407ebd4496779d
alexc957 <alexcoronel1995@gmail.com>
2019-08-14 16:31:48 -0500

    CORS added

--- api.py
+++ api.py
@@ -6,9 +6,11 @@ import os
 from flask_marshmallow import Marshmallow 
 import jwt 
 import datetime
-from functools import wraps
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources='/api/*')
 basedir = os.path.abspath(os.path.dirname(__file__)) 
 app.config['SECRET_KEY']='secretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///'+os.path.join(basedir,'vueAppDB.db')
@@ -18,18 +20,22 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app) 
 
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args,**kwargs):
-        token = None
-        if 'x-access-token' in requests.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            return jsonify({'message': 'Token is missing'}),401
-        try:
-            data = jwt.decode(token,app.config['SECRET_KEY'])
-        except:
-            print("")
+#def token_required(f):
+#    @wraps(f)
+#    def decorated(*args,**kwargs):
+#        token = None
+#        if 'x-access-token' in requests.headers:
+#            token = request.headers['x-access-token']
+#        if not token:
+#           return jsonify({'message': 'Token is missing'}),401
+#        try:
+#            data = jwt.decode(token,app.config['SECRET_KEY'])
+#            current_user = User.query.filter_by(public_id=data['public_id']).first()
+#        except:
+#            return jsonify({'message':'Invalid Token'}), 401
+#
+#        return f(current_user,*args,**kwargs)
+#    return decorated
 
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -53,21 +59,24 @@ user_schema = UserSchema(strict=True)
 users_schema = UserSchema(many=True,strict=True)
 
 
-@app.route('/user',methods=['GET'])
+@app.route('/api/user',methods=['GET'])
 def get_all_users():
     all_users = User.query.all()
     result = users_schema.dump(all_users)
 
     return jsonify(result.data)
 
-@app.route('/user/<public_id>',methods=['GET'])
+@app.route('/api/user/<public_id>',methods=['GET'])
+
+
 def get_one_user(public_id):
     user =  User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({"message":"No user found"})
     return user_schema.jsonify(user)
 
-@app.route('/user',methods=['POST'])
+@app.route('/api/user',methods=['POST'])
+
 def create_user():
     data = request.get_json()
     hashed_password = generate_password_hash(data.get('password'),method='sha256')
@@ -80,7 +89,8 @@ def create_user():
     db.session.commit()
     return jsonify({'message':'new user Created'})
 
-@app.route('/user/<public_id>',methods=['PUT'])
+@app.route('/api/user/<public_id>',methods=['PUT'])
+
 def promote_user(public_id):
     user =  User.query.filter_by(public_id=public_id).first()
     if not user:
@@ -89,7 +99,8 @@ def promote_user(public_id):
     db.session.commit()
     return jsonify({"message":"The user has been promoted"})
 
-@app.route('/user/<public_id>',methods=['DELETE'])
+@app.route('/api/user/<public_id>',methods=['DELETE'])
+
 def delete_user(public_id):
     user =  User.query.filter_by(public_id=public_id).first()
     if not user:
@@ -97,6 +108,7 @@ def delete_user(public_id):
     db.session.delete(user)
     db.session.commit()
     return jsonify({'message':'user has been deleted'})
+
 @app.route('/login')
 def login():
     auth = request.authorization 
